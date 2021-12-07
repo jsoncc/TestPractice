84ba55ec7fb2309f93229ef7094afe923fac0ca1
jsoncc <896415482@qq.com>
2021-12-07 17:29:04 +0800

    核心技术 卷I

---
+++ csdn/src/main/java/book/coretechnology/catalog/PL3_6_6.java
@@ -0,0 +1,20 @@
+package book.coretechnology.catalog;
+
+public class PL3_6_6 {
+    public static void main(String[] args) {
+        String greeting = "Hello";
+
+        int n = greeting.length();
+        System.out.println(n);
+
+        char first = greeting.charAt(0);
+        System.out.println(first);
+
+        int cpCount = greeting.codePointCount(0, greeting.length());
+        System.out.println(cpCount);
+
+        int index = greeting.offsetByCodePoints(0,4);
+        int cp =greeting.codePointAt(index);
+        System.out.println(cp);
+    }
+}
