0c6597770bba1865071fd6e7a71540d242f31655
jsoncc <896415482@qq.com>
2022-03-16 14:30:12 +0800

    核心技术 卷I

---
+++ csdn/src/main/java/book/coretechnology/catalog/Catalog3_8_5_2.java
@@ -0,0 +1,19 @@
+package book.coretechnology.catalog;
+
+/*
+ * 此类不做参考
+ * */
+import com.sun.javafx.css.Size;
+
+public class Catalog3_8_5_2 {
+    public static void main(String[] args) {
+        /*Size sz = "SMALL";
+        switch (sz) {
+            case SMALL:
+                // no need to use Size.SMALL
+
+                break;
+
+        }*/
+    }
+}
---
+++ csdn/src/main/java/book/coretechnology/catalog/Catalog3_8_6_2.java
@@ -0,0 +1,30 @@
+package book.coretechnology.catalog;
+
+import java.util.Scanner;
+/*
+* 此类不做参考
+* */
+public class Catalog3_8_6_2 {
+    public static void main0(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        int goal = 10;
+        while (sum < goal) {
+            System.out.print("Enter a number: ");
+            int n = in.nextInt();
+            if (n < 0) continue;
+            sum += n; // not executed if n < 0 }
+            System.out.println(sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int count = 1; count <= 100; count++) {
+            System.out.print("Enter a number, -1 to quit: ");
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            if (n < 0) continue;
+            sum += n; // not executed if n < 0 }
+        }
+    }
+}
